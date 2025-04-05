82ad6c4b5fc30194949a750519044e2f2138b8d9
Yae-Htet-Aung <yaetactaung@gmail.com>
2025-04-05 09:46:55 +0630

    baseURL changed

--- src/constants/endpoints.js
+++ src/constants/endpoints.js
@@ -1,7 +1,7 @@
-export const baseURL = "http://localhost:8000/api";
-export const imageURL = "http://localhost:8000";
-// export const baseURL = "https://api.shweyokelayexpress.com/api";
-// export const imageURL = "https://api.shweyokelayexpress.com";
+// export const baseURL = "http://localhost:8000/api";
+// export const imageURL = "http://localhost:8000";
+export const baseURL = "https://api.shweyotelayexpress.com/api";
+export const imageURL = "https://api.shweyotelayexpress.com";
 
 export const endpoints = {
   dashboard: "dashboard",
